573d5513836303ca1a3ee323de1e7d8d4b22a3ef
Maxime Charriere <51918753+maximecharriere@users.noreply.github.com>
2025-03-08 22:58:30 +0100

    Change resume (CV) to mine (2025)

--- src/components/menu.js
+++ src/components/menu.js
@@ -266,7 +266,7 @@ const Menu = () => {
               </ol>
             )}
 
-            <a href="/resume.pdf" className="resume-link">
+            <a href="/2025_CV-MaximeCharriere_EN.pdf" className="resume-link">
               Resume
             </a>
           </nav>
