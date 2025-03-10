981b0ef9ad8c23205e7041d0e4cef7ba6eb12f10
Maxime Charriere <51918753+maximecharriere@users.noreply.github.com>
2025-03-10 10:12:24 +0100

    Modify header logo

--- src/components/icons/hex.js
+++ src/components/icons/hex.js
@@ -1,20 +1,17 @@
 import React from 'react';
 
 const IconHex = () => (
-  <svg id="hex" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
+  <svg id="hex" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 97 97">
     <title>Hexagon</title>
-    <g transform="translate(-8.000000, -2.000000)">
-      <g transform="translate(11.000000, 5.000000)">
-        <polygon
-          stroke="currentColor"
-          strokeWidth="5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          points="39 0 0 22 0 67 39 90 78 68 78 23"
-          fill="currentColor"
-        />
-      </g>
-    </g>
+    <polygon
+      id="Shape"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      points="2.9 47.5 24.9 86.5 69.9 86.5 92.9 47.5 70.9 8.5 25.9 8.5 2.9 47.5"
+      fill="currentColor"
+    />
   </svg>
 );
 
