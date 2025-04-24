1e37ad431765099581f325fbc0ab653d9b99e555
Maxime Charriere <51918753+maximecharriere@users.noreply.github.com>
2025-04-24 20:05:40 +0200

    Replace old google-analytics with mine

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,4 +4,23 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
- // You can delete this file if you're not using it
+// You can delete this file if you're not using it
+
+import React from 'react';
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <script key="gtag-js" async src="https://www.googletagmanager.com/gtag/js?id=G-63907S7J5E" />,
+    <script
+      key="gtag-config"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-63907S7J5E');
+        `,
+      }}
+    />,
+  ]);
+};
